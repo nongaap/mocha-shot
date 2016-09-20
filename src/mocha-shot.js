25350a9f71f3411cd8b7c7bec18c37ef22415cf8
Mike Puangmalai <mike@nongaap.com>
2016-09-20 11:42:56 -0700

    formatting changes

--- src/mocha-shot.js
+++ src/mocha-shot.js
@@ -3,9 +3,10 @@ const fs = require('fs');
 const mkdirp = require('mkdirp');
 const escapeStringRegexp = require('escape-string-regexp');
 const path = require('path');
-const mochaShot = {};
 const appRootDir = require('app-root-dir').get();
-const pathToTestFile = path.relative(path.join(__dirname), path.join(appRootDir,'test/test.js'));
+
+const mochaShot = {};
+const pathToTestFile = path.relative(path.join(__dirname), path.join(appRootDir, 'test/test.js'));
 
 mochaShot.createTest = function (fileName) {
   if (fileName === undefined || typeof fileName !== 'string') {
@@ -14,17 +15,11 @@ mochaShot.createTest = function (fileName) {
 
   return fs.access(pathToTestFile, fs.F_OK, (err) => {
     if (!err) {
-      const requirePath = '\nvar ' + fileName + ' = require(' + JSON.stringify(path.relative(path.join(appRootDir,'test/'), module.parent.filename)) + ');\n';
-      console.log('filePath dirname',path.join(__filename));
-      console.log('filePath file',path.basename(__filename));
-      console.log('module.parent.filename',module.parent.filename);
-      console.log('from - to', path.relative(path.join(appRootDir,'test/'), module.parent.filename));
-      console.log('app root dir',appRootDir);
-      console.log('test path',path.join(appRootDir,'test/test.js'));
+      const requirePath = '\nvar ' + fileName + ' = require(' + JSON.stringify(path.relative(path.join(appRootDir, 'test/'), module.parent.filename)) + ');\n';
       const escapedString = escapeStringRegexp(requirePath);
       return fs.readFile(pathToTestFile, 'utf8', (error, data) => {
         if (!data.match(escapedString)) {
-          fs.appendFile(pathToTestFile, '\nvar ' + fileName + ' = require(' + JSON.stringify(path.relative(path.join(appRootDir,'test/'), module.parent.filename)) + ');\n');
+          fs.appendFile(pathToTestFile, '\nvar ' + fileName + ' = require(' + JSON.stringify(path.relative(path.join(appRootDir, 'test/'), module.parent.filename)) + ');\n');
           const test = ['\n', 'describe(', JSON.stringify('File should exist'), ',', ' function(){', '\n  it(', JSON.stringify('Check that the file being test exists'), ', function() {\n  expect(' + fileName + ').to.not.be.undefined;\n  });\n', '});\n'].join('');
           return fs.appendFile(pathToTestFile, test);
         }
@@ -35,7 +30,7 @@ mochaShot.createTest = function (fileName) {
 
       // creates test.js file and adds 'use strict' adds require('chai').expect
       fs.appendFile(pathToTestFile, JSON.stringify('use strict') + ';' + "\nvar expect = require('chai').expect;\n" + "\nvar request = require('supertest');\n");
-      fs.appendFile(pathToTestFile, '\nvar ' + fileName + ' = require(' + JSON.stringify(path.relative(path.join(appRootDir,'test/'), module.parent.filename)) + ');\n');
+      fs.appendFile(pathToTestFile, '\nvar ' + fileName + ' = require(' + JSON.stringify(path.relative(path.join(appRootDir, 'test/'), module.parent.filename)) + ');\n');
       const test = ['\n', 'describe(', JSON.stringify('File should exist'), ',', ' function(){', '\n  it(', JSON.stringify('Check that the file being test exists'), ', function() {\n  expect(' + fileName + ').to.not.be.undefined;\n  });\n', '});\n'].join('');
       return fs.appendFile(pathToTestFile, test);
     }
@@ -59,7 +54,7 @@ mochaShot.equal = function (des, input, expected_output) {
 };
 
 mochaShot.eql = function(des, inputObj, expected_outputObj) {
-  let test = ['\n', 'describe( ',JSON.stringify('Equality test for objects or arrays'), ',', ' function(){', '\n  it(', JSON.stringify(des), ', function() {\n  expect(' + JSON.stringify(inputObj) + ').to.eql(' + JSON.stringify(expected_outputObj) + ');\n  });\n', '});\n'].join('');
+  let test = ['\n', 'describe( ', JSON.stringify('Equality test for objects or arrays'), ',', ' function(){', '\n  it(', JSON.stringify(des), ', function() {\n  expect(' + JSON.stringify(inputObj) + ').to.eql(' + JSON.stringify(expected_outputObj) + ');\n  });\n', '});\n'].join('');
 
   let escapedString = escapeStringRegexp(test);
 
@@ -79,8 +74,8 @@ mochaShot.eqlFunction = function (description, input, expected_value, method) {
   let test = ['\n', 'describe( ', JSON.stringify('Testing functionality of a module method'), ',', ' function(){', '\n  it(', JSON.stringify(description), ', function() {\n  var input = ' + JSON.stringify(input) + ';\n  var expected = ' + JSON.stringify(expected_value) + ';\n  var actual = ' + method + '(input);\n  expect(actual).to.eql(expected);\n  });\n', '});\n'].join('');
 
   let escapedString = escapeStringRegexp(test);
-  
-  fs.readFile(pathToTestFile, 'utf8', (err, data) => {  
+
+  fs.readFile(pathToTestFile, 'utf8', (err, data) => {
     if (err) {
       return console.log('Error: Must create test file. Use mochaShot.createTest method (i.e. mochaShot.createTest("module_name")');
     }
@@ -90,12 +85,13 @@ mochaShot.eqlFunction = function (description, input, expected_value, method) {
   });
 };
 
+//API get test.
 mochaShot.getTest = function (description, route, input, expected, moduleName, statusCode) {
   if (typeof input !== 'string') { return console.log('Input must be passed in as a string'); }
   let test = ['\n', 'describe( ', JSON.stringify('API GET Test'), ',', ' function(){', '\n  it(', JSON.stringify(description), ', function(done) {\n    request(' + moduleName + ')\n      .get(' + JSON.stringify(route) + ')\n      .end(function(err, res){\n        expect(' + input + ').to.eql(' + JSON.stringify(expected) + ');\n        expect(res.statusCode).to.equal(' + statusCode + ');\n        done();\n      });\n  });\n});\n'].join('');
 
   let escapedString = escapeStringRegexp(test);
-  
+
   fs.readFile(pathToTestFile, 'utf8', (err, data) => {
     if (err) {
       return console.log('Error: Must create test file. Use mochaShot.createTest method (i.e. mochaShot.createTest("module_name")');
